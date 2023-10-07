877e4d85f554bfb0bf88aa1f0324ada3b08706ea
nathanieladiah <nathanieladiah@gmail.com>
2023-10-07 17:54:56 -0400

    Stop page from scrolling when side menu is open

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -1,5 +1,6 @@
 const nav = document.getElementById("primary-navigation");
 const navToggle = document.getElementById("nav-toggle");
+const body = document.querySelector("body");
 
 navToggle.onclick = () => {
   const visibility = nav.dataset.visible;
@@ -7,8 +8,10 @@ navToggle.onclick = () => {
   if (visibility === "false") {
     nav.dataset.visible = "true";
     navToggle.setAttribute("aria-expanded", true);
+    body.classList.add("noscroll");
   } else {
     nav.dataset.visible = "false";
     navToggle.setAttribute("aria-expanded", false);
+    body.classList.remove("noscroll");
   }
 };
